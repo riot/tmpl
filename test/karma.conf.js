8fbc51bed80849695bc306049a162b0bb3700e86
Gianluca Guarini <gianluca.guarini@gmail.com>
2015-08-23 21:30:19 +0200

    removed: saucelabs from the karma config file

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
 
     browsers: ['PhantomJS'],
 
-    reporters: ['progress', 'saucelabs', 'coverage'],
+    reporters: ['progress', 'coverage'],
     preprocessors: {
       '../dist/tmpl.riot.js': ['coverage']
     },

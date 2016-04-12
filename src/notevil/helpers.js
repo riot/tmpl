24f2cd90add4421ae481660f2da9727f37acbad6
Gianluca Guarini <gianluca.guarini@gmail.com>
2016-04-13 00:23:42 +0200

    updated: rollup config

--- src/notevil/helpers.js
+++ src/notevil/helpers.js
@@ -1,4 +1,4 @@
-export function getGlobal(str) {
-  var ctx = typeof window !== 'object' ? global : window
+export function getGlobal (str) {
+  var ctx = (typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : this)
   return typeof str !== 'undefined' ? ctx[str] : ctx
 }
